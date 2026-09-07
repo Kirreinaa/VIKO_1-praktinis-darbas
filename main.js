5084c11290e3515bb74b0e3a356b427c7698c959
Rivvka <malinauskaitejulija@gmail.com>
2026-09-07 08:26:32 +0300

    added header & footer, cards data

---
+++ main.js
@@ -0,0 +1,31 @@
+const cards = [
+    {
+        title: "outfit",
+        description: "new",
+        price: "9,99$",
+    },
+    {
+        title: "outfit1",
+        description: "new1",
+        price: "9,99$1",
+    },
+    {
+        title: "outfit2",
+        description: "new2",
+        price: "9,99$2",
+    },
+
+];
+
+const appleDevices = {
+    name: "ipad",
+    params: {
+        version: 1,
+    },
+}
+
+const cardsContainer = document.querySelector("#cards")
+
+console.log("text", appleDevices.params.version)
+
+{anime: ""}
